f9044a61195ea96032087a74db98fa664519af3a
Chris Kanich <kaytwo@gmail.com>
2017-10-18 20:48:44 -0500

    broken version with newline delimited uin extraction function

--- scrape.js
+++ scrape.js
@@ -2,8 +2,10 @@ const querystring = require('querystring');
 const puppeteer = require('puppeteer');
 const inquirer = require('inquirer');
 const fs = require('mz/fs');
+const XLSX = require('xlsx');
 
 const TEST_UIN = '674640002';
+// newline delimited list of UINs with no header
 const UIN_FILE = 'uins.txt';
 const START_PAGE = 'https://darsweb.admin.uillinois.edu:443/darswebadv_uic/servlet/EASDarsServlet';
 const USERNAME_SELECTOR = '#netid';
@@ -19,9 +21,18 @@ const CONTINUE_BUTTON = 'input[value="Continue"]';
 const DARS_REPORT_URL = "https://darsweb.admin.uillinois.edu/darswebadv_uic/bar"
 
 async function uins (fName) {
+
+    const workbook = XLSX.readFile("advisees.xlsx");
+    const ws = workbook.Sheets[workbook.SheetNames[0]];
+    return XLSX.utils.sheet_to_json(ws);
+
+    /*
+
+    Old newline delimited uin file
     const rawfile = await fs.readFile(fName);
-    const uins = rawfile.toString().split('\n').filter((x) => x.length > 0);
-    console.log(uins);
+    const uins = rawfile.toString().split('\n').filter((x) => x.length > 0 && x.indexOf("UIN") == -1);
+    return uins;
+    */
 }
 
 async function lp () {
@@ -38,13 +49,12 @@ async function lp () {
     }
 
 (async () => {
-    const browser = await puppeteer.launch(); // {headless: false});
+    const uin_list = await uins(UIN_FILE);
+    const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
+    await page.goto(START_PAGE);
 
     // new tab workaround from https://github.com/GoogleChrome/puppeteer/issues/386#issuecomment-336590028
-
-
-    await page.goto(START_PAGE);
     await page.evaluate(() => {
         window.open = (new_url) => {
             console.log("intercepted window.open");
@@ -71,54 +81,57 @@ async function lp () {
     
     // save off this url to go back to to repeat the process
     const STUDENT_SELECTION_URL = page.url(); 
+ 
+    for(uin_idx in uin_list) {
+        const this_uin = uin_list[uin_idx];
+        // student selection
+        await page.click(UIN_TEXTBOX_SELECTOR);
+        await page.keyboard.type(this_uin);
+        await page.click(CONTINUE_BUTTON);
+        await page.waitForNavigation();
+
+        // click audits dropdown
+        await page.click("#main > div > a:nth-child(5)");
+        // start new audit
+        await page.click("#menuAUDITS > a:nth-child(2)");
+        await page.waitForNavigation();
+
+        // start the default selected audit
+        await page.click('#SUBMITTABLE > tbody > tr:nth-child(2) > td > input[type="submit"]');
+        await page.waitForNavigation();
+
+        await page.waitForSelector('input[value="Open Audit"]');
+        /* alternate slow way
+        await page.waitFor(3000);
+        await page.click('input[value="Refresh List"]');
+        await page.waitForNavigation();
+        */  
+        let report_query  = await page.evaluate(() => {
+            var retval = {};
+            retval.instidq = document.querySelector('input[name="instidq"]').value;
+            retval.instid  = document.querySelector('input[name="instid"]').value;
+            retval.instcd  = document.querySelector('input[name="instcd"]').value;
+            retval.DETAILS = document.querySelector('input[name="DETAILS"]').value;
+            var sourcecode = document.querySelector('input[value="Open Audit"]').onclick.toString().split("'");
+            retval.job_id = sourcecode[1];
+            retval.int_seq_no = sourcecode[3];
+            return retval;
+        });
+        await page.goto(DARS_REPORT_URL + "?" + querystring.stringify(report_query));
+
+        // on the audit page - get printer friendly version
+        await page.click('#openAllLink');
+        
+        // await page.pdf({path: this_uin + '.pdf'});
+        await page.screenshot({path: this_uin + '.png'});
+
+        await page.goto(STUDENT_SELECTION_URL);
+    }
+
     
-    // student selection
-    await page.click(UIN_TEXTBOX_SELECTOR);
-    await page.keyboard.type(TEST_UIN);
-    await page.click(CONTINUE_BUTTON);
-    await page.waitForNavigation();
-
-    // click audits dropdown
-    await page.click("#main > div > a:nth-child(5)");
-    // start new audit
-    await page.click("#menuAUDITS > a:nth-child(2)");
-    await page.waitForNavigation();
-
-    // start the default selected audit
-    await page.click('#SUBMITTABLE > tbody > tr:nth-child(2) > td > input[type="submit"]');
-    await page.waitForNavigation();
-
-    await page.waitForSelector('input[value="Open Audit"]');
-    /* alternate slow way
-    await page.waitFor(3000);
-    await page.click('input[value="Refresh List"]');
-    await page.waitForNavigation();
-    */  
-    let report_query  = await page.evaluate(() => {
-        var retval = {};
-        retval.instidq = document.querySelector('input[name="instidq"]').value;
-        retval.instid  = document.querySelector('input[name="instid"]').value;
-        retval.instcd  = document.querySelector('input[name="instcd"]').value;
-        retval.DETAILS = document.querySelector('input[name="DETAILS"]').value;
-        var sourcecode = document.querySelector('input[value="Open Audit"]').onclick.toString().split("'");
-        retval.job_id = sourcecode[1];
-        retval.int_seq_no = sourcecode[3];
-        return retval;
-    });
-    await page.goto(DARS_REPORT_URL + "?" + querystring.stringify(report_query));
-    // await page.waitForNavigation();
-
-    // on the audit page - get printer friendly version
-    // await page.waitFor(1500);
-    await page.click('#openAllLink');
-    await page.pdf({path: TEST_UIN + '.pdf'});
-    // await page.screenshot({path: TEST_UIN + '.png'});
-
-
     
     
-    
-    // await browser.close();
+    await browser.close();
 })();
 
 /*
